db28f6e4a2e77dad2e076e25aef17e7895e44935
uniquelylost <jsneider24@gmail.com>
2020-04-30 20:38:51 -0400

    reorder filter

--- navbar/navbar-variants.php
+++ navbar/navbar-variants.php
@@ -230,14 +230,14 @@ class Navbar_Variants {
 			$items .= $nav_button;
 		}
 
-		// Check for filter.
-		if ( has_filter( 'conversions_navbar_below_extras' ) ) {
-			$items = apply_filters( 'conversions_navbar_below_extras', $items );
-		}
-
 		// If not empty add wrapper.
 		if ( ! empty( $items ) ) {
 			$items = '<ul class="list-inline nav-extras">' . $items . '</ul>';
+		}
+
+		// Check for filter before output.
+		if ( has_filter( 'conversions_navbar_below_extras' ) ) {
+			$items = apply_filters( 'conversions_navbar_below_extras', $items );
 		}
 
 		return $items;
