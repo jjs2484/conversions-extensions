7fd1eb26d5a707c1bdbf0cf8904c78ae5f552306
Jesse S <3428918+jjs2484@users.noreply.github.com>
2020-11-29 02:25:12 -0500

    refactor navbar variant

--- navbar/Navbar_Variants.php
+++ navbar/Navbar_Variants.php
@@ -146,118 +146,35 @@ class Navbar_Variants {
 		// Create empty string variable to add active elements to.
 		$items = '';
 
-		// Is woocommerce is active?
-		if ( class_exists( 'woocommerce' ) ) {
+		$woocommerce = \conversions\Navbar::conversions_navbar_woocommerce();
+		$edd         = \conversions\Navbar::conversions_navbar_edd();
+		$bbpress     = \conversions\Navbar::conversions_navbar_bbp();
+		$search      = \conversions\Navbar::conversions_navbar_search();
+		$button      = \conversions\Navbar::conversions_navbar_button();
 
-			// Append WooCommerce Cart icon?
-			if ( get_theme_mod( 'conversions_wc_cart_nav', true ) === true ) {
-				// output the cart icon with item count.
-				$cart_link = sprintf(
-					'<li class="cart list-inline-item">%s</li>',
-					\conversions\WooCommerce::get_cart_nav_html()
-				);
-				// Add the cart icon to the end of the menu.
-				$items = $cart_link;
-			}
-
-			// Append WooCommerce Account icon?
-			if ( get_theme_mod( 'conversions_wc_account', false ) === true ) {
-
-				if ( is_user_logged_in() ) {
-					$wc_al = __( 'My Account', 'conversions' );
-				} else {
-					$wc_al = __( 'Login / Register', 'conversions' );
-				}
-				// output the account icon if active.
-				$wc_account_link = sprintf(
-					'<li class="account-icon list-inline-item"><a href="%1$s" class="nav-link" title="%2$s"><i aria-hidden="true" class="fas fa-user"></i><span class="sr-only">%2$s</span></a></li>',
-					esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ),
-					$wc_al
-				);
-
-				// Add the account to the end of the menu.
-				$items .= $wc_account_link;
-			}
+		if ( ! empty( $woocommerce ) ) {
+			$items .= $woocommerce;
 		}
 
-		// Is Easy Digital Downloads active?
-		if ( class_exists( 'Easy_Digital_Downloads' ) ) {
-
-			// Append Easy Digital Downloads Cart icon?
-			if ( get_theme_mod( 'conversions_edd_nav_cart', true ) === true ) {
-
-				$edd_cart_totals = sprintf(
-					'<span class="header-cart edd-cart-quantity">%s</span><span class="sr-only">' . __( 'View your shopping cart', 'conversions' ) . '</span>',
-					edd_get_cart_quantity()
-				);
-
-				// output the cart icon with item count.
-				$edd_cart_link = sprintf(
-					'<li class="cart list-inline-item"><a title="' . __( 'View your shopping cart', 'conversions' ) . '" class="cart-customlocation nav-link" href="%s"><i aria-hidden="true" class="fas fa-shopping-cart"></i>%s</a></li>',
-					esc_url( edd_get_checkout_uri() ),
-					$edd_cart_totals
-				);
-
-				// Add the cart icon to the end of the menu.
-				$items .= $edd_cart_link;
-			}
-
-			// Append Easy Digital Downloads Account icon?
-			if ( get_theme_mod( 'conversions_edd_nav_account', false ) === true ) {
-
-				if ( is_user_logged_in() ) {
-					$edd_al = __( 'My Account', 'conversions' );
-				} else {
-					$edd_al = __( 'Login / Register', 'conversions' );
-				}
-				// output the account icon if active.
-				$edd_account_link = sprintf(
-					'<li class="account-icon list-inline-item"><a href="%1$s" class="nav-link" title="%2$s"><i aria-hidden="true" class="fas fa-user"></i><span class="sr-only">%2$s</span></a></li>',
-					esc_url( edd_get_user_verification_page() ),
-					$edd_al
-				);
-
-				// Add the account to the end of the menu.
-				$items .= $edd_account_link;
-			}
+		if ( ! empty( $edd ) ) {
+			$items .= $edd;
 		}
 
-		// Append Search Icon to nav? Separate function coversions_nav_search_modal adds modal html to footer.
-		if ( get_theme_mod( 'conversions_nav_search_icon', false ) === true ) {
-			$nav_search = sprintf(
-				'<li class="search-icon list-inline-item"><a href="#csearchModal" data-toggle="modal" class="nav-link" title="%1$s"><i aria-hidden="true" class="fas fa-search"></i><span class="sr-only">%1$s</span></a></li>',
-				__( 'Search', 'conversions' )
-			);
-
-			// Add the nav button to the end of the menu.
-			$items .= $nav_search;
+		if ( ! empty( $bbpress ) ) {
+			$items .= $bbpress;
 		}
 
-		// Append Navigation Button?
-		if ( get_theme_mod( 'conversions_nav_button', 'no' ) !== 'no' ) {
-
-			$nav_btn_text = get_theme_mod( 'conversions_nav_button_text' );
-			if ( empty( $nav_btn_text ) ) {
-				$nav_btn_text = '';
-			}
-			$nav_btn_url = get_theme_mod( 'conversions_nav_button_url' );
-			if ( empty( $nav_btn_url ) ) {
-				$nav_btn_url = '';
-			}
-
-			$nav_button = sprintf(
-				'<li class="nav-callout-button list-inline-item"><a title="%1$s" href="%2$s" class="btn %3$s">%1$s</a></li>',
-				esc_html( $nav_btn_text ),
-				esc_url( $nav_btn_url ),
-				esc_attr( get_theme_mod( 'conversions_nav_button' ) )
-			);
-
-			// Add the nav button to the end of the menu.
-			$items .= $nav_button;
+		if ( ! empty( $search ) ) {
+			$items .= $search;
 		}
 
-		// If not empty add wrapper.
+		if ( ! empty( $button ) ) {
+			$items .= $button;
+		}
+
+		// If not empty change item classes and add wrapper.
 		if ( ! empty( $items ) ) {
+			$items = str_replace( 'menu-item nav-item', 'list-inline-item', $items );
 			$items = '<ul class="list-inline nav-extras">' . $items . '</ul>';
 		}
 
